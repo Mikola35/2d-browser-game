05276d18827eaf4d1669ef4514ca13e52f8fc12f
Nick M <nik@mamadaevv.ru>
2025-03-09 23:31:53 +0200

    Update development and deployment scripts in package.json; adjust static file serving in server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Раздача статических файлов из папки src
-app.use(express.static(path.join(__dirname, 'src')));
+// Раздача статических файлов из корневой папки
+app.use(express.static(path.join(__dirname)));
 
 // Запуск сервера
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
- 
